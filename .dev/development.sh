6a90bd2767a63d656f6e44719d326cbd3aae13d2
Tomasz Czerwiński <tomasz.czerwinski.1997@gmail.com>
2023-09-27 21:52:43 +0200

    ♻️ Update development script and fix config eslint error

--- .dev/development.sh
+++ .dev/development.sh
@@ -2,9 +2,17 @@
 
 DEV_DIR=$(cd "$(dirname "$0")" || exit 1;  pwd -P)
 
-if ! command -v mongod > /dev/null; then
+if [ -d "$(command -v mongod)" ]; then
   echo "Please install MongoDB mongod to run the mongo database locally"
   exit 1
 fi
 
-sudo systemctl start mongod
+APP_PATH=$(realpath "$DEV_DIR/../app")
+
+if ! cd $APP_PATH; then
+  echo "Cannot change directory to $APP_PATH"; exit 1;
+fi
+
+cd $APP_PATH && 
+sudo systemctl start mongod &&
+npm run dev
